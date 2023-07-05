f77ecf2045151d07cea327aed6cb932c46830d56
Mateo <frisded555@gmail.com>
2023-07-04 20:36:29 -0500

    countries.json updated with the same countries as countries-map. Two countries deleted

--- src/components/MapChart.jsx
+++ src/components/MapChart.jsx
@@ -1,9 +1,9 @@
 import {
-    ComposableMap,
-    Geographies,
-    Geography,
-    Annotation,
-    ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography,
+  Annotation,
+  ZoomableGroup,
 } from "react-simple-maps";
 
 import { Tooltip } from "react-tooltip";
@@ -16,52 +16,46 @@ import geoUrl from "../data/countries-map.json";
 /* const anotherURl = "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/africa.json" */
 
 function MapChart() {
-    /* const [content, setContent] = useState(""); */
+  /* const [content, setContent] = useState(""); */
 
-    return (
-        <div className="flex flex-col justify-center items-center ">
-            <Tooltip id="tooltip" />
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <Tooltip id="tooltip" />
 
-            <div className="border-2  bg-white w-full ">
-                <ComposableMap data-tip="">
-                    <ZoomableGroup center={[6, -32]} zoom={0.85}>
-                        <Geographies geography={geoUrl}>
-                            {({ geographies }) =>
-                                geographies.map((geo) => (
-                                    <Geography
-                                        data-tooltip-id="tooltip"
-                                        data-tooltip-content={
-                                            geo.properties.name
-                                        } /* {content} */
-                                        data-tooltip-place="top"
-                                        data-tooltip-float="true"
-                                        key={geo.rsmKey}
-                                        geography={geo}
-                                        fill="#0092CA"
-                                        className="hover:fill-[#F53]"
-                                        // available props: id, properties : {name, ["Alpha-2"]}
-                                        onClick={() =>
-                                            (document.getElementById(
-                                                "gameContainer"
-                                            ).textContent =
-                                                document.getElementById(
-                                                    "gameContainer"
-                                                ).textContent +
-                                                ", " +
-                                                geo.properties.name)
-                                        }
-                                        /* onMouseEnter={() => {
+      <div className="border-2  bg-white w-full ">
+        <ComposableMap data-tip="">
+          <ZoomableGroup center={[6, -32]} zoom={0.85}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    data-tooltip-id="tooltip"
+                    data-tooltip-content={geo.id} /* {content} */
+                    data-tooltip-place="top"
+                    data-tooltip-float="true"
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#0092CA"
+                    className="hover:fill-[#F53]"
+                    // available props: id, properties : {name, ["Alpha-2"]}
+                    onClick={() =>
+                      (document.getElementById("gameContainer").textContent =
+                        document.getElementById("gameContainer").textContent +
+                        ", " +
+                        geo.properties.name)
+                    }
+                    /* onMouseEnter={() => {
                                             const { name } = geo.properties;
                                             setContent(`${name}`);
                                         }}
                                         onMouseLeave={() => {
                                             setContent("");
                                         }} */
-                                    />
-                                ))
-                            }
-                        </Geographies>
-                        {/* <Annotation
+                  />
+                ))
+              }
+            </Geographies>
+            {/* <Annotation
                             subject={[1.5218, 42.5063]}
                             dx={-90}
                             dy={-30}
@@ -81,11 +75,11 @@ function MapChart() {
                                 {"Andorra"}
                             </text>
                         </Annotation> */}
-                    </ZoomableGroup>
-                </ComposableMap>
-            </div>
-        </div>
-    );
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    </div>
+  );
 }
 
 export default MapChart;
