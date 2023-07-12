f8b0353c19e3e1a5e01343844cdd93a9585fe1f1
Mateo <frisded555@gmail.com>
2023-07-12 09:22:01 -0500

    Save card style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,14 @@
+import LayoutCard from "../components/LayoutCard";
+
 function Home() {
   return (
     <div className="bg-pageColor ">
-      <section className="flex flex-wrap gap-14 justify-evenly px-[15%] py-[5%]">
-        <div className="map-card">
-          <div className="map-card-content">World</div>
-        </div>
-        <div className="map-card">
-          <div className="map-card-content">Americas</div>
-        </div>
-        <div className="map-card">
-          <div className="map-card-content">Asia</div>
-        </div>
-        <div className="map-card">
-          <div className="map-card-content">Europe</div>
-        </div>
-        <div className="map-card">
-          <div className="map-card-content">Africa</div>
-        </div>
+      <section className="flex flex-wrap gap-14 justify-evenly px-[13%] py-[5%]">
+        <LayoutCard name="World" imgPath="/world.svg"/>
+        <LayoutCard name="Americas" imgPath="/americas.svg"/>
+        <LayoutCard name="Asia" imgPath="/asia.svg"/>
+        <LayoutCard name="Europe" imgPath="/europe.svg"/>
+        <LayoutCard name="Africa" imgPath="/africa.svg"/>
       </section>
     </div>
   );
