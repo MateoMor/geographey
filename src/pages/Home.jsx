1d65ce72ec4a76b916ce5471e5ca8725289d1169
Mateo <frisded555@gmail.com>
2023-07-12 22:01:03 -0500

    Github pages test, attemp 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ function Home() {
   return (
     <div className="bg-pageColor ">
       <section className="flex flex-wrap gap-14 justify-evenly px-[11%] py-[5%]">
-        <LayoutCard name="World" imgPath="/maps-images/world.svg"/>
-        <LayoutCard name="Americas" imgPath="/maps-images/americas.svg"/>
-        <LayoutCard name="Asia" imgPath="/maps-images/asia.svg"/>
-        <LayoutCard name="Europe" imgPath="/maps-images/europe.svg"/>
-        <LayoutCard name="Africa" imgPath="/maps-images/africa.svg"/>
+        <LayoutCard name="World" imgPath="/geographey/maps-images/world.svg"/>
+        <LayoutCard name="Americas" imgPath="/geographey/maps-images/americas.svg"/>
+        <LayoutCard name="Asia" imgPath="/geographey/maps-images/asia.svg"/>
+        <LayoutCard name="Europe" imgPath="/geographey/maps-images/europe.svg"/>
+        <LayoutCard name="Africa" imgPath="/geographey/maps-images/africa.svg"/>
       </section>
     </div>
   );
